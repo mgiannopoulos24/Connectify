5b1c5fc2f70e83e49b33ceefca0f739ba99cc6c8
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-08-24 21:16:49 +0300

    Maybe fixed

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -57,7 +57,6 @@ config :esbuild,
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
 # Configure tailwind (the version is required)
