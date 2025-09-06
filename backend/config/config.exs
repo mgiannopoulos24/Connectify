9456d4ab7573fe855cf0fc60988ad579e54ca888
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-09-06 15:15:58 +0300

    Update lock

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -61,7 +61,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.7",
+  version: "4.1.12",
   backend: [
     args: ~w(
       --input=assets/css/app.css
